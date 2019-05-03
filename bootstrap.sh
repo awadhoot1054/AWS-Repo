f9bab0fb4e76582a43577de2256ab24c115a6f2e
awadhoot1054 <awadhoot.altekar@gmail.com>
2019-05-03 13:10:18 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum update -y 
 yum install httpd -y
-echo "<html><body><center><h1>Hello Cloud Gurus. Here is my web page!</h1></center></body></html>" > /var/www/html/index.html
+echo "<html><body><center><h1>Here is my web page!</h1></center></body></html>" > /var/www/html/index.html
 systemctl start httpd
 systemctl enable httpd
 
